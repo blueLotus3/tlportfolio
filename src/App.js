eed1c8ec42cf4cab121e437fed85502ec23cbc5b
Troy Lemons Jr <troylemonsjr@gmail.com>
2025-10-08 12:37:12 -0700

    added browserrouter to handle routes within app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; 
+import Home from './components/Home.js'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>This is my Portfolio Page</p>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import react from 'react'
+
+
+const Home = () => {
+    return (
+        <div className="heading">
+            <p>This is the Heading Part</p>
+        </div>
+    )
+}
+
+export default Home;
+
