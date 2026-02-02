c27cb08f0bcc76518f71ab062b72ecafccce5f90
Troy Lemons Jr <troylemonsjr@gmail.com>
2026-02-02 11:42:00 -0800

    added imgs that represent my developer skills

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,27 @@ const Home = () => {
 
     return (
     <div className="Home">
-     <h2>Troy Lemons || Software Developer</h2>
+     <h2>Hi, I'm Troy Lemons</h2>
+     <p>I'm a Fullstack Developer</p>
+
+
+     <div className="skillsSection">
+        <h3 className="skillsHeader">My Skills</h3>
+        <ul className="skills">
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1770060949/hml_tv5rwv.png" alt="html5 skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1770061008/css_hr91zv.png" alt="css skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1770061093/js_k3lbhp.png" alt="js skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1609030925/download_soap9c.png" alt="github skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1770060868/react1_czmq4p.png" alt="react skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1616016836/symbol-ruby_njuag9.jpg" alt="ruby skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1616016848/symbol-mongodb_fouzcl.jpg" alt="mongodb skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1616016853/symbol-postgresql_lfynqh.png" alt="postgresql skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1770060247/c_ruidh9.png" alt="c++ skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1770060380/java_anhqwh.png" alt="java skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1770060477/python_q5etxa.png" alt="python skill" width="100" height="100"/></li>
+            <li className="skill"><img src="https://res.cloudinary.com/ds7w3ysag/image/upload/v1770060561/swift_gbm61g.jpg" alt="swift skill" width="100" height="100"/></li>
+        </ul>
+     </div>
      </div>
      
     )
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,7 +4,20 @@ const Projects = () => {
 
     return (
         <div className="projContent">
-            <p>Here are the Projects Page</p>
+            <h4>Here are some projects</h4>
+            <ul>
+        <li><a href="https://pandajitsu.netlify.app">
+            <iframe id="frame1" title="Martial Arts Store" src="https://pandajitsu.netlify.app"  scrolling="no"></iframe>
+            <p>PandaJitsu Martial Arts Store</p>
+        </a></li>
+        <li>
+        <a href="https://beerlabs.netlify.app">
+            <iframe id="frame1" title="Martial Arts Store" src="https://beerlabs.netlify.app"  scrolling="no"></iframe>
+            <p>BeerLabs Menu</p>
+        </a>
+        </li>
+        </ul>
+
         </div>
     )
 }
