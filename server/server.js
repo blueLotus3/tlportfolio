63474323b5996797e01f13b63763f4c0eb5332b1
Troy Lemons Jr <troylemonsjr@gmail.com>
2026-06-02 17:08:31 -0700

    modified PORTS

--- server/server.js
+++ server/server.js
@@ -65,6 +65,8 @@ const transporter = nodemailer.createTransport({
     }
   });
 
-  app.listen(5000, () => {
-    console.log("Server running on port 5000");
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
